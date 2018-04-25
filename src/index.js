eb7733237635bda24c36551f26afdad1e8d578e3
Robert Buels <rbuels@gmail.com>
2018-04-25 11:45:45 -0700

    initial commit of skeleton

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import {
+  parseStream,
+  parseFile,
+  parseStringSync,
+  formatSync,
+  formatStream,
+  formatFile,
+} from './api'
+
+import * as util from './util'
+
+export default {
+  parseStream,
+  parseFile,
+  parseStringSync,
+  formatSync,
+  formatStream,
+  formatFile,
+  util,
+}
---
+++ test/parse.test.js
@@ -0,0 +1,7 @@
+import fs from 'fs'
+import cram from '../src'
+
+describe('CRAM reader', () => {
+  it('can read a cram file without crashing', async () => {
+  })
+})
