5992fed6e72b0a846862e610623e05b01b2fe54d
Robert Buels <rbuels@gmail.com>
2018-05-11 09:03:52 -0700

    add genEntriesForRange method to cramindex class

--- src/cramIndex.js
+++ src/cramIndex.js
@@ -14,13 +14,14 @@ class CramIndex {
   constructor(readFile) {
     // read the whole thing, then un-gzip it
     this.readFile = readFile
-    this.lines = this.parseIndex()
+    this.index = this.parseIndex()
   }
 
   parseIndex() {
+    const index = []
     return this.readFile()
       .then(data => gunzip(data))
-      .then(uncompressedBuffer =>
+      .then(uncompressedBuffer => {
         uncompressedBuffer
           .toString('utf8')
           .split(/\r?\n/)
@@ -28,21 +29,76 @@ class CramIndex {
           .filter(
             line => line && line[0] !== undefined && !Number.isNaN(line[0]),
           )
-          .map(
-            ([seqId, start, span, containerStart, sliceStart, sliceBytes]) => ({
-              seqId,
-              start,
-              span,
-              containerStart,
-              sliceStart,
-              sliceBytes,
-            }),
-          ),
-      )
+          .forEach(
+            ([seqId, start, span, containerStart, sliceStart, sliceBytes]) => {
+              if (!index[seqId]) index[seqId] = []
+
+              index[seqId].push({
+                start,
+                span,
+                containerStart,
+                sliceStart,
+                sliceBytes,
+              })
+            },
+          )
+
+        // sort each of them by start
+        index.forEach((entries, i) => {
+          index[i] = entries.sort(
+            (a, b) => a.start - b.start || a.span - b.span,
+          )
+        })
+        return index
+      })
   }
 
-  getLines() {
-    return this.lines
+  getIndex() {
+    return this.index
+  }
+
+  async getEntriesForRange(seqId, queryStart, queryEnd) {
+    const seqEntries = (await this.index)[seqId]
+    if (!seqEntries) return []
+    const len = seqEntries.length
+
+    // binary search to find an entry that
+    // overlaps the range, then extend backward
+    // and forward from that
+    let searchPosition = Math.floor(len / 2)
+    const compare = entry => {
+      const entryStart = entry.start
+      const entryEnd = entry.start + entry.span
+      if (entryStart >= queryEnd) return -1 // entry is ahead of query
+      if (entryEnd <= queryStart) return 1 // entry is behind query
+      return 0 // entry overlaps query
+    }
+    let nextSearchDirection
+    do {
+      nextSearchDirection = compare(seqEntries[searchPosition])
+      if (nextSearchDirection > 0) {
+        if (searchPosition === len - 1)
+          // we are at the right end and have found no overlapping ranges
+          return []
+        searchPosition = Math.floor((searchPosition + len) / 2)
+      } else if (nextSearchDirection < 0) {
+        if (searchPosition === 0)
+          // we are all the way at the left end and have found no overlapping ranges
+          return []
+        searchPosition = Math.floor(searchPosition / 2)
+      }
+    } while (nextSearchDirection)
+
+    // now extend backward
+    let overlapStart = searchPosition
+    while (overlapStart && !compare(seqEntries[overlapStart - 1]))
+      overlapStart -= 1
+    // and then extend forward
+    let overlapEnd = searchPosition
+    while (overlapEnd < len - 1 && !compare(seqEntries[overlapEnd + 1]))
+      overlapEnd += 1
+
+    return seqEntries.slice(overlapStart, overlapEnd + 1)
   }
 }
 
--- test/crai.test.js
+++ test/crai.test.js
@@ -5,19 +5,72 @@ const { testDataFile } = require('./util')
 const CramIndex = require('../src/cramIndex')
 
 describe('.crai reader', () => {
-  it('can read auxf#values.tmp.cram.crai', async () => {
-    const file = testDataFile('auxf#values.tmp.cram.crai')
+  it('can read xx#unsorted.tmp.cram.crai', async () => {
+    const file = testDataFile('xx#unsorted.tmp.cram.crai')
     const index = new CramIndex(file.readFile.bind(file))
-    const lines = await index.getLines()
-    expect(lines).to.deep.equal([
-      {
-        seqId: 0,
-        start: 1,
-        span: 20,
-        containerStart: 295,
-        sliceStart: 1042,
-        sliceBytes: 1989,
-      },
+    let data = await index.getIndex()
+    expect(data).to.deep.equal([
+      [
+        {
+          start: 1,
+          span: 20,
+          containerStart: 562,
+          sliceStart: 143,
+          sliceBytes: 200,
+        },
+        {
+          start: 1,
+          span: 20,
+          containerStart: 923,
+          sliceStart: 173,
+          sliceBytes: 243,
+        },
+      ],
+      [
+        {
+          start: 1,
+          span: 10,
+          containerStart: 923,
+          sliceStart: 173,
+          sliceBytes: 243,
+        },
+        {
+          start: 11,
+          span: 10,
+          containerStart: 252,
+          sliceStart: 181,
+          sliceBytes: 111,
+        },
+      ],
+    ])
+
+    expect(await index.getEntriesForRange(2, 0, 0)).to.deep.equal([])
+    expect(await index.getEntriesForRange(-1, 9, 9)).to.deep.equal([])
+    expect(await index.getEntriesForRange(0, 100, 300)).to.deep.equal([])
+    expect(await index.getEntriesForRange(0, -100, -80)).to.deep.equal([])
+    expect(await index.getEntriesForRange(0, 0, 20)).to.deep.equal(data[0])
+    expect(await index.getEntriesForRange(0, 1, 21)).to.deep.equal(data[0])
+    expect(await index.getEntriesForRange(1, 0, 20)).to.deep.equal(data[1])
+
+    data = [
+      [
+        { start: 1, span: 1 },
+        { start: 100, span: 1 },
+        { start: 101, span: 1 },
+        { start: 102, span: 1 },
+        { start: 300, span: 1 },
+        { start: 400, span: 10 },
+        { start: 404, span: 1 },
+        { start: 410, span: 1 },
+      ],
+    ]
+    index.index = Promise.resolve(data)
+    expect(await index.getEntriesForRange(0, 1, 2)).to.deep.equal([data[0][0]])
+    expect(await index.getEntriesForRange(0, 1, 100)).to.deep.equal([
+      data[0][0],
+    ])
+    expect(await index.getEntriesForRange(0, 100, 101)).to.deep.equal([
+      data[0][1],
     ])
   })
 })
