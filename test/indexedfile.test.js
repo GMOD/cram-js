ad365a1a7f93ed3d24083cc7b0a67f04a0a5f14b
Robert Buels <rbuels@gmail.com>
2018-06-09 13:09:08 -0700

    add a parse-without-errors test for all of the rest of the cram files

--- test/indexedfile.test.js
+++ test/indexedfile.test.js
@@ -47,4 +47,70 @@ describe('.crai indexed cram file', () => {
     // )
     expect(features).to.deep.equal(expectedFeatures3)
   })
+  ;[
+    'auxf#values.tmp.cram',
+    'c1#bounds.tmp.cram',
+    'c1#clip.tmp.cram',
+    'c1#noseq.tmp.cram',
+    'c1#pad1.tmp.cram',
+    'c1#pad2.tmp.cram',
+    'c1#pad3.tmp.cram',
+    'c1#unknown.tmp.cram',
+    'c2#pad.tmp.cram',
+    'ce#1.tmp.cram',
+    'ce#1000.tmp.cram',
+    'ce#2.tmp.cram',
+    'ce#5.tmp.cram',
+    'ce#5b.tmp.cram',
+    'ce#large_seq.tmp.cram',
+    'ce#supp.tmp.cram',
+    'ce#tag_depadded.tmp.cram',
+    'ce#tag_padded.tmp.cram',
+    'ce#unmap.tmp.cram',
+    'ce#unmap1.tmp.cram',
+    'ce#unmap2.tmp.cram',
+    'headernul.tmp.cram',
+    'md#1.tmp.cram',
+    'sam_alignment.tmp.cram',
+    'xx#blank.tmp.cram',
+    'xx#large_aux.tmp.cram',
+    'xx#large_aux2.tmp.cram',
+    'xx#minimal.tmp.cram',
+    'xx#pair.tmp.cram',
+    'xx#repeated.tmp.cram',
+    'xx#rg.tmp.cram',
+    'xx#tlen.tmp.cram',
+    'xx#tlen2.tmp.cram',
+    'xx#triplet.tmp.cram',
+    'xx#unsorted.tmp.cram',
+  ].forEach(filename => {
+    it(`can read the first chrom of ${filename} without error`, async () => {
+      const cram = new IndexedCramFile({
+        cram: testDataFile(filename),
+        index: new CramIndex(testDataFile(`${filename}.crai`)),
+      })
+
+      const features = await cram.getFeaturesForRange(0, 0, Infinity)
+      // require('fs').writeFileSync(
+      //   'test/data/ce#1000.tmp.cram.test1.expected.json',
+      //   JSON.stringify(features, null, '  '),
+      // )
+      // console.log(`${filename} first ref got ${features.length} features`)
+      expect(features.length).to.be.greaterThan(-1)
+    })
+    it(`can read the second chrom of ${filename} without error`, async () => {
+      const cram = new IndexedCramFile({
+        cram: testDataFile(filename),
+        index: new CramIndex(testDataFile(`${filename}.crai`)),
+      })
+
+      const features = await cram.getFeaturesForRange(1, 0, Infinity)
+      // require('fs').writeFileSync(
+      //   'test/data/ce#1000.tmp.cram.test1.expected.json',
+      //   JSON.stringify(features, null, '  '),
+      // )
+      // console.log(`${filename} second ref got ${features.length} features`)
+      expect(features.length).to.be.greaterThan(-1)
+    })
+  })
 })
