5201cbcd34c5a60c50de156765d974d4705c0bab
Robert Buels <rbuels@gmail.com>
2018-07-01 23:48:38 -0700

    add a bunch more flag accessors to cramrecord class

--- src/cramFile/record.js
+++ src/cramFile/record.js
@@ -8,23 +8,70 @@ class CramRecord {
     this.tags = {}
   }
 
-  /** */
+  /**
+   * the read is paired in sequencing, no matter whether it is mapped in a pair
+   */
+  isPaired() {
+    return !!(this.flags & Constants.BAM_FPAIRED)
+  }
+
+  //  the read is mapped in a proper pair
+  isProperlyPaired() {
+    return !!(this.flags & Constants.BAM_FPROPER_PAIR)
+  }
+
+  /** the read itself is unmapped; conflictive with isProperlyPaired */
   isSegmentUnmapped() {
     return !!(this.flags & Constants.BAM_FUNMAP)
   }
 
-  /** */
+  /** the read itself is unmapped; conflictive with isProperlyPaired */
+  isMateUnmapped() {
+    return !!(this.flags & Constants.BAM_FMUNMAP)
+  }
+
+  /** the read is mapped to the reverse strand */
   isReverseComplemented() {
     return !!(this.flags & Constants.BAM_FREVERSE)
   }
 
-  /** */
+  /** the mate is mapped to the reverse strand */
   isMateReverseComplemented() {
     return !!(this.flags & Constants.BAM_FMREVERSE)
   }
 
+  /** */
+  isRead1() {
+    return !!(this.flags & Constants.BAM_FREAD1)
+  }
+
+  /** */
+  isRead2() {
+    return !!(this.flags & Constants.BAM_FREAD2)
+  }
+
+  /** */
+  isSecondary() {
+    return !!(this.flags & Constants.BAM_FSECONDARY)
+  }
+
+  /** */
+  isFailedQc() {
+    return !!(this.flags & Constants.BAM_FQCFAIL)
+  }
+
+  /** optical or PCR duplicate */
+  isDuplicate() {
+    return !!(this.flags & Constants.BAM_FDUP)
+  }
+
+  /** */
+  isSupplementary() {
+    return !!(this.flags & Constants.BAM_FSUPPLEMENTARY)
+  }
+
   /**
-   * @returns true if the
+   *
    */
   isDetached() {
     return !!(this.cramFlags & Constants.CRAM_FLAG_DETACHED)
