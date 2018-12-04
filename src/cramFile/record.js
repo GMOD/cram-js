0ef22f72db1e10f5ded1ad752aa997ee36c47a87
Jim Robinson <jrobinso@broadinstitute.org>
2018-12-03 21:24:19 -0800

    Fix for mate strand problem with 1KG files

--- src/cramFile/record.js
+++ src/cramFile/record.js
@@ -152,7 +152,11 @@ class CramRecord {
 
   /** @returns {boolean} true if the mate is mapped to the reverse strand */
   isMateReverseComplemented() {
-    return !!(this.flags & Constants.BAM_FMREVERSE)
+    if(this.mate && undefined !== this.mate.flags) {
+      return !!(this.mate.flags && Constants.CRAM_M_REVERSE);
+    } else {
+      return !!(this.flags & Constants.BAM_FMREVERSE)
+    }
   }
 
   /** @returns {boolean} true if this is read number 1 in a pair */
---
+++ test/1kg-mate.test.js
@@ -0,0 +1,45 @@
+const {expect} = require('chai')
+const {IndexedCramFile, CramFile, CraiIndex} = require('../src')
+
+describe('1kg mate test', () => {
+  describe('readme 1', () => {
+
+    it('runs without error', async () => {
+      const messages = []
+      const console = {
+        log(msg) {
+          messages.push(msg)
+        },
+      }
+
+      const indexedCramFile = new IndexedCramFile({
+        cram: new CramFile({
+          url: 'https://s3.amazonaws.com/1000genomes/data/HG00096/alignment/HG00096.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram',
+          seqFetch: async (seqId, start, end) => {
+            let fakeSeq = ''
+            for (let i = start; i <= end; i += 1) {
+              fakeSeq += 'A'
+            }
+            return fakeSeq
+          },
+          checkSequenceMD5: false
+        }),
+        index: new CraiIndex({
+          url: 'https://s3.amazonaws.com/1000genomes/data/HG00096/alignment/HG00096.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram.crai'
+        })
+      })
+
+      //chr8:128,749,421-128,749,582
+      const records = await indexedCramFile.getRecordsForRange(7, 128749421, 128749582)
+
+      let i=0;
+
+      const downstreamMateRecord = records.filter(rec => 'SRR062634.138050' === rec.readName)[0]
+      expect(downstreamMateRecord.isMateReverseComplemented()).to.equal(true)
+
+      const detachedMateRecord = records.filter(rec => 'SRR062635.19801940' === rec.readName)[0]
+      expect(detachedMateRecord.isMateReverseComplemented()).to.equal(true)
+
+    }).timeout(10000);
+  })
+})
