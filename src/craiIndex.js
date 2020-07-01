e23e79d638deac90251eb147e4dffc3eff23266d
Colin <colin.diesh@gmail.com>
2020-07-01 16:10:38 -0400

    Add abortable-promise-cache for error recovery

--- src/craiIndex.js
+++ src/craiIndex.js
@@ -1,3 +1,6 @@
+const AbortablePromiseCache = require('abortable-promise-cache').default
+const QuickLRU = require('quick-lru')
+
 const { promisify } = require('es6-promisify')
 const zlib = require('zlib')
 
@@ -56,79 +59,77 @@ class CraiIndex {
    */
   constructor(args) {
     const filehandle = open(args.url, args.path, args.filehandle)
+    this._parseCache = new AbortablePromiseCache({
+      cache: new QuickLRU({ maxSize: 1 }),
+      fill: (data, signal) => this.parseIndex({ signal }),
+    })
     this.readFile = filehandle.readFile.bind(filehandle)
   }
 
   parseIndex() {
     const index = {}
-    if (!this.index) {
-      this.index = this.readFile()
-        .then(data => {
-          if (data[0] === 31 && data[1] === 139) return gunzip(data)
-          return data
-        })
-        .then(uncompressedBuffer => {
+    return this.readFile()
+      .then(data => {
+        if (data[0] === 31 && data[1] === 139) return gunzip(data)
+        return data
+      })
+      .then(uncompressedBuffer => {
+        if (
+          uncompressedBuffer.length > 4 &&
+          uncompressedBuffer.readUInt32LE(0) === BAI_MAGIC
+        ) {
+          throw new CramMalformedError(
+            'invalid .crai index file. note: file appears to be a .bai index. this is technically legal but please open a github issue if you need support',
+          )
+        }
+        // interpret the text as regular ascii, since it is
+        // supposed to be only digits and whitespace characters
+        // this is written in a deliberately low-level fashion for performance,
+        // because some .crai files can be pretty large.
+        let currentRecord = []
+        let currentString = ''
+        for (let i = 0; i < uncompressedBuffer.length; i += 1) {
+          const charCode = uncompressedBuffer[i]
           if (
-            uncompressedBuffer.length > 4 &&
-            uncompressedBuffer.readUInt32LE(0) === BAI_MAGIC
+            (charCode >= 48 && charCode <= 57) /* 0-9 */ ||
+            (!currentString && charCode === 45) /* leading - */
           ) {
-            throw new CramMalformedError(
-              'invalid .crai index file. note: file appears to be a .bai index. this is technically legal but please open a github issue if you need support',
-            )
-          }
-          // interpret the text as regular ascii, since it is
-          // supposed to be only digits and whitespace characters
-          // this is written in a deliberately low-level fashion for performance,
-          // because some .crai files can be pretty large.
-          let currentRecord = []
-          let currentString = ''
-          for (let i = 0; i < uncompressedBuffer.length; i += 1) {
-            const charCode = uncompressedBuffer[i]
-            if (
-              (charCode >= 48 && charCode <= 57) /* 0-9 */ ||
-              (!currentString && charCode === 45) /* leading - */
-            ) {
-              currentString += String.fromCharCode(charCode)
-            } else if (charCode === 9 /* \t */) {
-              currentRecord.push(Number.parseInt(currentString, 10))
-              currentString = ''
-            } else if (charCode === 10 /* \n */) {
-              currentRecord.push(Number.parseInt(currentString, 10))
-              currentString = ''
-              addRecordToIndex(index, currentRecord)
-              currentRecord = []
-            } else if (
-              charCode !== 13 /* \r */ &&
-              charCode !== 32 /* space */
-            ) {
-              // if there are other characters in the file besides
-              // space and \r, something is wrong.
-              throw new CramMalformedError('invalid .crai index file')
-            }
-          }
-
-          // if the file ends without a \n, we need to flush our buffers
-          if (currentString) {
+            currentString += String.fromCharCode(charCode)
+          } else if (charCode === 9 /* \t */) {
             currentRecord.push(Number.parseInt(currentString, 10))
-          }
-          if (currentRecord.length === 6) {
+            currentString = ''
+          } else if (charCode === 10 /* \n */) {
+            currentRecord.push(Number.parseInt(currentString, 10))
+            currentString = ''
             addRecordToIndex(index, currentRecord)
+            currentRecord = []
+          } else if (charCode !== 13 /* \r */ && charCode !== 32 /* space */) {
+            // if there are other characters in the file besides
+            // space and \r, something is wrong.
+            throw new CramMalformedError('invalid .crai index file')
           }
+        }
 
-          // sort each of them by start
-          Object.entries(index).forEach(([seqId, ent]) => {
-            index[seqId] = ent.sort(
-              (a, b) => a.start - b.start || a.span - b.span,
-            )
-          })
-          return index
+        // if the file ends without a \n, we need to flush our buffers
+        if (currentString) {
+          currentRecord.push(Number.parseInt(currentString, 10))
+        }
+        if (currentRecord.length === 6) {
+          addRecordToIndex(index, currentRecord)
+        }
+
+        // sort each of them by start
+        Object.entries(index).forEach(([seqId, ent]) => {
+          index[seqId] = ent.sort(
+            (a, b) => a.start - b.start || a.span - b.span,
+          )
         })
-    }
-    return this.index
+        return index
+      })
   }
 
-  getIndex() {
-    return this.parseIndex()
+  getIndex(opts = {}) {
+    return this._parseCache.get('index', null, opts.signal)
   }
 
   /**
@@ -137,7 +138,7 @@ class CraiIndex {
    * the given reference sequence ID, false otherwise
    */
   async hasDataForReferenceSequence(seqId) {
-    return !!(await this.index)[seqId]
+    return !!(await this.getIndex())[seqId]
   }
 
   /**
@@ -152,7 +153,7 @@ class CraiIndex {
    * `{start, span, containerStart, sliceStart, sliceBytes }`
    */
   async getEntriesForRange(seqId, queryStart, queryEnd) {
-    const seqEntries = (await this.index)[seqId]
+    const seqEntries = (await this.getIndex())[seqId]
     if (!seqEntries) return []
     const len = seqEntries.length
 
