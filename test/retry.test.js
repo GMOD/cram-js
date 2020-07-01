5f6c7190bbffe9e1a3a5733914685e55be8a2a77
Colin <colin.diesh@gmail.com>
2020-07-01 17:19:23 -0400

    Add test for behavior

--- test/retry.test.js
+++ test/retry.test.js
@@ -10,6 +10,7 @@ describe('retry nonexist file', () => {
   it('file moves', async () => {
     let cram
     mock({})
+    let exception = 0
     try {
       cram = new IndexedCramFile({
         cramFilehandle: new LocalFile('./test/data/ce#tag_padded.tmp.cram'),
@@ -21,11 +22,35 @@ describe('retry nonexist file', () => {
       await cram.cram.getSamHeader()
     } catch (e) {
       /* console.error('initial error', e) */
+      exception = 1
     }
+    expect(exception).to.equal(1)
 
     mock.restore()
     const ret = await cram.cram.getSamHeader()
 
     expect(ret[0].tag).to.equal('HD')
+  })
+  it('index moves', async () => {
+    let cram
+    mock({})
+    let exception = 0
+    try {
+      cram = new IndexedCramFile({
+        cramFilehandle: new LocalFile('./test/data/ce#tag_padded.tmp.cram'),
+        index: new CraiIndex({
+          filehandle: new LocalFile('./test/data/ce#tag_padded.tmp.cram.crai'),
+        }),
+      })
+
+      await cram.getRecordsForRange(0, 2, 200)
+    } catch (e) {
+      exception = 1
+    }
+    expect(exception).to.equal(1)
+
+    mock.restore()
+    const features = await cram.getRecordsForRange(0, 2, 200)
+    expect(features.length).to.equal(8)
   })
 })
