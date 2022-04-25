a7ea7c327929efd05dc19d3f36edb97d575fdb91
Colin <colin.diesh@gmail.com>
2022-04-25 13:40:50 -0600

    Back to main

--- src/sam.ts
+++ src/sam.ts
@@ -1,12 +1,13 @@
 export function parseHeaderText(text: string) {
-  return text.split(/\r?\n/).map(line => {
-    const [tag, ...fields] = line.split(/\t/)
-    return {
+  return text
+    .split(/\r?\n/)
+    .map(line => line.split('\t'))
+    .filter(f => !!f[0])
+    .map(([tag, ...fields]) => ({
       tag: tag.slice(1),
       data: fields.map(f => {
         const [fieldTag, value] = f.split(':', 2)
         return { tag: fieldTag, value }
       }),
-    }
-  })
+    }))
 }
