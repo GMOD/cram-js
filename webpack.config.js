222772d1e7894f235e55b8a938f009892f824e05
Colin <colin.diesh@gmail.com>
2022-05-16 13:00:44 -0600

    Add test for off-by-one

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,12 @@ const path = require('path')
 module.exports = {
   mode: 'production',
   entry: './dist/index.js',
+  resolve: {
+    fallback: {
+      buffer: require.resolve('buffer/'),
+      url: require.resolve('url/'),
+    },
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'cram-bundle.js',
