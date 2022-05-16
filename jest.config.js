8410bc71498fe747fe94df4ee84c921ea150598b
Colin Diesh <colin.diesh@gmail.com>
2022-05-16 14:04:33 -0600

    Update to use webpack 5 (#112)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|js)x?$': 'ts-jest',
   },
