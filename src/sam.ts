488333bfe5ec21c2160d53ca8c7ccff892473cbc
Colin Diesh <colin.diesh@gmail.com>
2024-02-20 13:33:28 -0500

    Update sam header parsing to handle colons in data (#132)

--- src/sam.ts
+++ src/sam.ts
@@ -1,20 +1,36 @@
-export type HeaderDataItem = {
-  tag: string
-  data: Array<{ tag: string; value: string }>
-}
-
-export function parseHeaderText(text: string): HeaderDataItem[] {
+export function parseHeaderText(text: string) {
   const lines = text.split(/\r?\n/)
-  const data: HeaderDataItem[] = []
-  lines.forEach(line => {
+  const data: {
+    tag: string
+    data: {
+      tag: string
+      value: string | undefined
+    }[]
+  }[] = []
+  for (const line of lines) {
     const [tag, ...fields] = line.split(/\t/)
-    const parsedFields = fields.map(f => {
-      const [fieldTag, value] = f.split(':', 2)
-      return { tag: fieldTag, value }
-    })
     if (tag) {
-      data.push({ tag: tag.substr(1), data: parsedFields })
+      data.push({
+        tag: tag.slice(1),
+        data: fields.map(f => {
+          const r = f.indexOf(':')
+          if (r !== -1) {
+            return {
+              tag: f.slice(0, r),
+              value: f.slice(r + 1),
+            }
+          } else {
+            // @CO lines are not comma separated.
+            // See "samtools view -H c2\#pad.3.0.cram"
+            // so, just store value tag itself
+            return {
+              tag: f,
+              value: undefined,
+            }
+          }
+        }),
+      })
     }
-  })
+  }
   return data
 }
