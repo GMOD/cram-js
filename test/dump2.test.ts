0baea2fcbb112b90a9e051672aa23e02a3c07ab9
Colin Diesh <colin.diesh@gmail.com>
2024-02-20 13:10:20 -0500

    Convert custom snapshot system to jest snapshots (#133)

---
+++ test/dump2.test.ts
@@ -0,0 +1,24 @@
+//@ts-nocheck
+import { t2 as testFileList } from './lib/testFileList'
+import { testDataFile } from './lib/util'
+import { dumpWholeFile } from './lib/dumpFile'
+import { CramFile } from '../src/index'
+import { FetchableSmallFasta } from './lib/fasta'
+
+describe('dumping cram files', () => {
+  testFileList.forEach(filename => {
+    it(`can dump the whole ${filename} without error`, async () => {
+      let seqFetch
+      if (filename.includes('#')) {
+        const referenceFileName = filename.replace(/#.+$/, '.fa')
+        const fasta = new FetchableSmallFasta(testDataFile(referenceFileName))
+        seqFetch = fasta.fetch.bind(fasta)
+      }
+
+      const filehandle = testDataFile(filename)
+      const file = new CramFile({ filehandle, seqFetch })
+      const fileData = await dumpWholeFile(file)
+      expect(fileData).toMatchSnapshot()
+    }, 10000)
+  })
+})
