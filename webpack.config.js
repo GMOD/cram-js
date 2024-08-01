8ac919158ccdf13343d058207473e46e5effd3c3
Colin <colin.diesh@gmail.com>
2024-08-01 09:42:07 -0400

    Fix buffer build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
+const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
   mode: 'production',
   entry: './dist/index.js',
+  plugins: [
+    // this is needed to properly polyfill buffer in desktop, after the CRA5
+    // conversion it was observed cram, twobit, etc that use
+    // @gmod/binary-parser complained of buffers not being real buffers
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
   resolve: {
     fallback: {
       buffer: require.resolve('buffer/'),
