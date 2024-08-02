ef9788589135e2670fd2be33f6ce0cf93a1bc996
Colin <colin.diesh@gmail.com>
2024-08-01 20:25:25 -0400

    Add window.Buffer=Buffer for igv.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { Buffer } from 'buffer'
+
+window.Buffer = Buffer
 export { default as CramFile, CramRecord } from './cramFile'
 export { default as CraiIndex } from './craiIndex'
 export { default as IndexedCramFile } from './indexedCramFile'
