6407213ed22db1180dc2fe913ad34017f9ad02cb
Colin <colin.diesh@gmail.com>
2025-11-19 16:45:53 -0500

    Add hyperfine benchmarking infrastructure

--- benchmarks/benchmark-runner.mjs
+++ benchmarks/benchmark-runner.mjs
@@ -2,19 +2,18 @@
 // Simple benchmark runner that can be called repeatedly by hyperfine
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
-import pkg from '../dist/index.js'
-import CraiIndex from '../dist/craiIndex.js'
-
-const { IndexedCramFile } = pkg
+import { LocalFile } from 'generic-filehandle2'
+import { IndexedCramFile } from '../esm/index.js'
+import CraiIndex from '../esm/craiIndex.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const cramPath = join(__dirname, '../test/data/HG002_ONTrel2_16x_RG_HP10xtrioRTG.cram')
 const craiPath = join(__dirname, '../test/data/HG002_ONTrel2_16x_RG_HP10xtrioRTG.cram.crai')
 
 const cram = new IndexedCramFile({
-  cramFilehandle: cramPath,
+  cramFilehandle: new LocalFile(cramPath),
   index: new CraiIndex({
-    filehandle: craiPath,
+    filehandle: new LocalFile(craiPath),
   }),
 })
 
