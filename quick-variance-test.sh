5c6c442c88e539a9f67ec35ebf764698d32823fe
Colin <colin.diesh@gmail.com>
2025-11-19 00:20:41 -0500

    Rm

--- quick-variance-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Running 3 profiles on current branch to measure variance..."
-echo ""
-
-for i in 1 2 3; do
-  echo "Run $i:"
-  yarn test profile 2>&1 | grep "test/profile.test.ts"
-  echo ""
-done
