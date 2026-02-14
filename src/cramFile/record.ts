7d7fe353d72b47b8c72a51906c7e7782498bd061
Colin <colin.diesh@gmail.com>
2026-02-14 11:20:04 -0500

    Improve pair orientation speed with further bit hacks

--- src/cramFile/record.ts
+++ src/cramFile/record.ts
@@ -3,6 +3,17 @@ import CramContainerCompressionScheme from './container/compressionScheme.ts'
 
 import type decodeRecord from './slice/decodeRecord.ts'
 
+// precomputed pair orientation strings indexed by ((flags >> 4) & 0xF) | (isize > 0 ? 16 : 0)
+// bits 0-3 encode flag bits 0x10(reverse),0x20(mate reverse),0x40(read1),0x80(read2)
+// bit 4 encodes whether isize > 0
+// prettier-ignore
+const PAIR_ORIENTATION_TABLE = [
+  'F F ','F R ','R F ','R R ','F2F1','F2R1','R2F1','R2R1',
+  'F1F2','F1R2','R1F2','R1R2','F2F1','F2R1','R2F1','R2R1',
+  'F F ','R F ','F R ','R R ','F1F2','R1F2','F1R2','R1R2',
+  'F2F1','R2F1','F2R1','R2R1','F1F2','R1F2','F1R2','R1R2',
+]
+
 export interface RefRegion {
   start: number
   end: number
@@ -411,29 +422,17 @@ export default class CramRecord {
   }
 
   // adapted from igv.js
-  // inlines flag checks and uses template literal instead of array+join
+  // uses precomputed lookup table indexed by flag bits + isize sign
   getPairOrientation() {
     const f = this.flags
     // combined check: paired (0x1) set, unmapped (0x4) clear, mate unmapped (0x8) clear
     if ((f & 0xd) !== 0x1 || this.sequenceId !== this.mate?.sequenceId) {
       return undefined
     }
-    const s1 = f & 0x10 ? 'R' : 'F'
-    const s2 = f & 0x20 ? 'R' : 'F'
-    let o1 = ' '
-    let o2 = ' '
-    if (f & 0x40) {
-      o1 = '1'
-      o2 = '2'
-    } else if (f & 0x80) {
-      o1 = '2'
-      o2 = '1'
-    }
-
     const isize = this.templateLength || this.templateSize || 0
-    return isize > 0
-      ? `${s1}${o1}${s2}${o2}`
-      : `${s2}${o2}${s1}${o1}`
+    return PAIR_ORIENTATION_TABLE[
+      ((f >> 4) & 0xf) | (isize > 0 ? 16 : 0)
+    ]
   }
 
   /**
