08be920b63b65d186bd0ff1330a94071d20f5289
Colin <colin.diesh@gmail.com>
2026-05-19 15:05:08 -0400

    Bumps

--- htscodecs-wasm/version.h
+++ htscodecs-wasm/version.h
@@ -1,3 +1,3 @@
 /* version.h for htscodecs WASM build */
-/* Updated from https://github.com/samtools/htscodecs commit 7dd27f4b2bfe0ffdce413337972b3ad68550c3bf */
+/* Updated from https://github.com/samtools/htscodecs commit 295a940d87eb6f5a9ca8c0fe58fb98a45bca31df */
 #define HTSCODECS_VERSION_TEXT "1.6.6"
